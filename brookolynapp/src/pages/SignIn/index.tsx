744210033b125097e6285d73c9f7212d7edd52e6
Douglas <douglas.lisboa@edu.unipar.br>
2020-09-16 19:40:00 -0300

    Adicionado components

--- brookolynapp/src/pages/SignIn/index.tsx
+++ brookolynapp/src/pages/SignIn/index.tsx
@@ -4,11 +4,21 @@ import { Container,  Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 import { Image } from 'react-native';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
 const SignIn: React.FC = () => {
     return(
         <Container>
             <Image source={logoImg}/>
             <Title>Faça seu logon</Title>
+
+            <Input name='email' icon='mail' placeholder='E-mail'/>
+
+            <Input name='password' icon='lock' placeholder='Senha'/>
+
+            <Button onPress={() => {}}>Entrar</Button>
         </Container>
     );
 };
