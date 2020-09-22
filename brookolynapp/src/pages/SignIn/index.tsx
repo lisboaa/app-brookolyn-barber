e76972def62dd3a16a6ac103aefdb9c63d460fdc
Douglas <douglas.lisboa@edu.unipar.br>
2020-09-22 10:41:06 -0300

    Adicionado Keyborad para identificar qual platform será utilizada e aplicar o style especifico; Adicionado ScrollView para caso de ter mais conteudo na tela ser possivel realizar a rolagem da view.

--- brookolynapp/src/pages/SignIn/index.tsx
+++ brookolynapp/src/pages/SignIn/index.tsx
@@ -1,25 +1,58 @@
 import React from 'react';
 
-import { Container,  Title } from './styles';
+import { 
+    Container,
+    Title,
+    ForgotPassword,
+    ForgotPasswordText,
+    CreateAccountButton,
+    CreateAccountButtonText
+} from './styles';
+
+import Icon from 'react-native-vector-icons/Feather'; 
 
 import logoImg from '../../assets/logo.png';
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignIn: React.FC = () => {
     return(
-        <Container>
-            <Image source={logoImg}/>
-            <Title>Faça seu logon</Title>
+        <>
+        <KeyboardAvoidingView
+            style={{ flex:1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+            enabled
+        >
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{ flex: 1 }}
+            >
+                <Container>
+                    <Image source={logoImg}/>
+                    <View>
+                        <Title>Faça seu logon</Title>
+                    </View>
 
-            <Input name='email' icon='mail' placeholder='E-mail'/>
+                    <Input name='email' icon='mail' placeholder='E-mail'/>
 
-            <Input name='password' icon='lock' placeholder='Senha'/>
+                    <Input name='password' icon='lock' placeholder='Senha'/>
 
-            <Button onPress={() => {}}>Entrar</Button>
-        </Container>
+                    <Button onPress={() => {}}>Entrar</Button>
+
+                    <ForgotPassword>
+                        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+                    </ForgotPassword>
+                </Container>
+            </ScrollView>
+        </KeyboardAvoidingView>
+
+        <CreateAccountButton onPress={() => {}}>
+            <Icon name='log-in' size={20} color="#ff9000"/>
+            <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+        </>
     );
 };
 
