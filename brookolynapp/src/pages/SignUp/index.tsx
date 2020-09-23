624506bcfc7eef8a2ba5517c5f01ce98ad81058b
Douglas <douglas.lisboa@edu.unipar.br>
2020-09-23 11:51:18 -0300

    Implementando ref nos inputs para utilização do utilizada next.

--- brookolynapp/src/pages/SignUp/index.tsx
+++ brookolynapp/src/pages/SignUp/index.tsx
@@ -10,7 +10,7 @@ import {
 import Icon from 'react-native-vector-icons/Feather'; 
 
 import logoImg from '../../assets/logo.png';
-import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView, TextInput } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -22,8 +22,10 @@ import { FormHandles } from '@unform/core';
 
 const SignIn: React.FC = () => {
     const navgation = useNavigation();
-
     const formRef = useRef<FormHandles>(null);
+
+    const emailInputRef = useRef<TextInput>(null);
+    const passwordInputRef = useRef<TextInput>(null);
 
     return(
         <>
@@ -43,11 +45,41 @@ const SignIn: React.FC = () => {
                     </View>
                     <Form ref={formRef} onSubmit={(data) => {console.log(data);
                     }}>
-                        <Input name='name' icon='user' placeholder='Nome'/>
+                        <Input
+                            autoCapitalize='words'
+                            name='name' 
+                            icon='user' 
+                            placeholder='Nome'
+                            returnKeyType='next'
+                            onSubmitEditing={() => {
+                                emailInputRef.current?.focus();
+                            }}
+                        />
                         
-                        <Input name='email' icon='mail' placeholder='E-mail'/>
+                        <Input
+                            ref={emailInputRef}
+                            keyboardType='email-address'
+                            autoCorrect={false}
+                            autoCapitalize='none'
+                            name='email' 
+                            icon='mail' 
+                            placeholder='E-mail'
+                            returnKeyType='next'
+                            onSubmitEditing={() => {
+                                passwordInputRef.current?.focus();
+                            }}
+                        />
 
-                        <Input name='password' icon='lock' placeholder='Senha'/>
+                        <Input
+                            ref={passwordInputRef}
+                            name='password' 
+                            icon='lock' 
+                            placeholder='Senha'
+                            textContentType='newPassword'
+                            returnKeyType='send'
+                            secureTextEntry
+                            onSubmitEditing={() => formRef.current?.submitForm()}
+                        />
                     </Form>
 
                     <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
