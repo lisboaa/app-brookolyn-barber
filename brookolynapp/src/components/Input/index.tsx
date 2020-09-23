403d9a77632782c768484be71108274df32bf23e
Douglas <douglas.lisboa@edu.unipar.br>
2020-09-23 12:28:56 -0300

    Implementando stylização nos inputs, focu com bordas.

--- brookolynapp/src/components/Input/index.tsx
+++ brookolynapp/src/components/Input/index.tsx
@@ -1,4 +1,11 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import React, { 
+    useEffect,
+    useRef,
+    useImperativeHandle,
+    forwardRef,
+    useState,
+    useCallback
+} from 'react';
 
 import { useField } from '@unform/core';
 import { TextInputProps } from 'react-native';
@@ -25,6 +32,19 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
     const { registerField, defaultValue = '', fieldName, error } = useField(name);
     const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
     
+    const [ isFocused, setIsFocused ] = useState(false);
+    const [ isFilled, setIsFilled ] = useState(false);
+
+    const handlerInputFocus = useCallback(() => {
+        setIsFocused(true);
+    }, []);
+
+    const hanlderInputBlur = useCallback(() => {
+        setIsFocused(false);
+
+        setIsFilled(!!inputValueRef.current.value);
+    },[]);
+
     useImperativeHandle(ref, () => ({
         focus() {
             inputElementRef.current.focus();
@@ -49,10 +69,12 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
     }, [fieldName, registerField]);
 
     return(
-    <Container>
-        <Icon name={icon} size={20} color='#666360'/>
+    <Container isFocused={isFocused}>
+        <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'}/>
 
         <TextInput
+            onFocus={handlerInputFocus}
+            onBlur={hanlderInputBlur}
             ref={inputElementRef}
             keyboardAppearance='dark'
             placeholderTextColor='#666360'
